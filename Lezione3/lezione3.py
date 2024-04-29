c28ecad3651a467283af81265ce16a3d538d74d6
Silviu-Savciuc <savciucsilviu@gmail.com>
2024-04-30 00:11:36 +0200

    4-14

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -130,6 +130,86 @@ primi_10_cubi = [numero ** 3 for numero in range(1, 11)]
 # Print dei primi 10 cubi
 print("I primi 10 cubi sono:", primi_10_cubi)
 
+"""
+4-10. Slices: Using one of the programs you wrote in this chapter, add several lines to the end of the program that do the following:
+• Print the message The first three items in the list are:. Then use a slice to print the first three items from that program’s list.
+• Print the message Three items from the middle of the list are:. Then use a slice to print three items from the middle of the list.
+• Print the message The last three items in the list are:. Then use a slice to print the last three items in the list.
+
+"""
+# list comprehension per generare una lista dei primi 10 cubi
+primi_10_cubi = [numero ** 3 for numero in range(1, 11)]
+
+# stampa dei primi 3 elementi della lista 
+print("i primi 3 elementi:", primi_10_cubi[:3])
+
+# stampa dei primi 3 elementi nel mezzo 
+mezzo = len(primi_10_cubi) // 2
+print("i primi 3 elementi:", primi_10_cubi[mezzo:mezzo+3])
+
+# stampa degli ultimi 3 elementi della lista 
+print("i primi 3 elementi:", primi_10_cubi[-3:])
+
+"""
+4-11. My Pizzas, Your Pizzas: Start with your program from Exercise 4-1. Make a copy of the list of pizzas, and call it friend_pizzas. 
+Then, do the following:
+• Add a new pizza to the original list.
+• Add a different pizza to the list friend_pizzas.
+• Prove that you have two separate lists. Print the message My favorite pizzas are:, 
+and then use a for loop to print the first list. Print the message 
+My friend’s favorite pizzas are:, and then use a for loop to print the second list. 
+Make sure each new pizza is stored in the appropriate list.
+
+"""
+#lista friend basata su pizz es.4-1
+friend_pizzas = pizzas[:]
+
+#pizza aggiunta nella lista originale
+pizzas.append("Margherita")
+
+#pizza aggiunta nella friend_pizzas
+friend_pizzas.append("Boscaiola")
+
+#lista le mie pizze preferite 
+print("le mie pizze preferite sono:")
+for pizza in pizzas:
+    print(pizza)
+    
+#lista friend_pizzas
+print("le sue pizze preferite sono:")
+for pizza in friend_pizzas:
+    print(pizza)
 
 
+"""    
+4-12. More Loops: All versions of foods.py in this section have avoided using for loops when printing, 
+to save space. 
+Choose a version of foods.py, 
+and write two for loops to print each list of foods.
+"""
 
+cibi={
+    'italiano':['pizza','pasta','gnocchi'],
+    'Giapponese':['sushi', 'ramen', 'tempura'],
+    'Moldavo':['mămăligă', 'sarmale', 'plăcinte']
+}
+
+#stampa della lista cibi
+
+# Stampa della lista di cibi
+print("\nCibi italiani:")
+for food in cibi['italiano']:
+    print(food)
+
+print("\nCibi Giapponesi:")
+for food in cibi['Giapponese']:
+    print(food)
+
+print("\nCibi Moldavi :")
+for food in cibi['Moldavo']:
+    print(food)
+
+"""
+4-14. PEP 8: Look through the original PEP 8 style guide at https://peps.python.org/pep-0008/. 
+You won’t use much of it now, but it might be interesting to skim through it.
+"""
